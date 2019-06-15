518b04359b937cd5da35f8c172ccd8807240cf64
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2019-06-15 22:17:06 +0800

    Update dataset_factory.py

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -23,11 +23,12 @@ from datasets import imagenet
 
 from datasets import pascalvoc_2007
 from datasets import pascalvoc_2012
-
+from datasets import flowers
 datasets_map = {
     'cifar10': cifar10,
     'imagenet': imagenet,
     'pascalvoc_2007': pascalvoc_2007,
+    'flowers':flowers,
     'pascalvoc_2012': pascalvoc_2012,
 }
 
